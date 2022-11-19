fe7985803daf1421b9c78c4c0a8e9b092399c1df
Snehal979 <bansodsnehal535@gmail.com>
2022-11-19 23:08:19 +0530

    queues linked list

--- Program.cs
+++ Program.cs
@@ -4,41 +4,44 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("1.push \n2.display \n3.pop \n 4.peek \n5,empty\n 6.exit");
-            int choice = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("1.push \n2.display \n3.pop \n 4.peek \n5,empty\n 6.exit");
+            //int choice = Convert.ToInt32(Console.ReadLine());
            
            
-            StackLinkedList stackLinkedList = new StackLinkedList();
-            stackLinkedList.Push(70);
-            stackLinkedList.Push(30);
-            stackLinkedList.Push(56);
+            //StackLinkedList stackLinkedList = new StackLinkedList();
+            //stackLinkedList.Push(70);
+            //stackLinkedList.Push(30);
+            //stackLinkedList.Push(56);
             
-            switch (choice)
-            {
-                    case 1:
-                        stackLinkedList.Push(70);
-                        stackLinkedList.Push(30);
-                        stackLinkedList.Push(56);
-                    stackLinkedList.Display();
-                    break;
-                    case 2:
-                        stackLinkedList.Display();
-                        break;
-                    case 3:
-                        stackLinkedList.Pop();
-                        stackLinkedList.Display();
-                        break;
-                    case 4:
-                        stackLinkedList.peek();
-                        break;
-                    case 5:
-                        stackLinkedList.Empty();
-                        break;
+            //switch (choice)
+            //{
+            //        case 1:
+            //            stackLinkedList.Push(70);
+            //            stackLinkedList.Push(30);
+            //            stackLinkedList.Push(56);
+            //        stackLinkedList.Display();
+            //        break;
+            //        case 2:
+            //            stackLinkedList.Display();
+            //            break;
+            //        case 3:
+            //            stackLinkedList.Pop();
+            //            stackLinkedList.Display();
+            //            break;
+            //        case 4:
+            //            stackLinkedList.peek();
+            //            break;
+            //        case 5:
+            //            stackLinkedList.Empty();
+            //            break;
                     
-            }
+            //}
             
-           
-
+           QueueLinkedList queueLinkedList = new QueueLinkedList();
+            queueLinkedList.Equeue(56);
+            queueLinkedList.Equeue(30);
+            queueLinkedList.Equeue(70);
+            queueLinkedList.Display();
             
         }
     }
---
+++ QueueLinkedList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedStackAndQueProblem
+{
+    public class QueueLinkedList
+    {
+        Node headtop;
+        public void Equeue(int data)
+        {
+            Node node = new Node(data);
+            if (this.headtop == null)
+            {
+                this.headtop = node;
+            }
+            else
+            {
+                Node temp = headtop;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into Linked List", node.data);
+        }
+        public void Display()
+        {
+            Node temp = this.headtop;
+            if (temp ==null)
+            {
+                Console.WriteLine("stacklist is empty");
+            }
+            while (temp !=null)
+            {
+                Console.WriteLine(temp.data +" ");
+                temp =temp.next;
+            }
+        }
+
+
+
+    }
+}
