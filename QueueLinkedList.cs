78e6e9b615536180c52050080110ef58d407c1d2
snehal979 <113917713+snehal979@users.noreply.github.com>
2022-11-19 23:21:35 +0530

    LinkedListProgram

---
+++ QueueLinkedList.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedStackAndQueProblem
+{
+    public class QueueLinkedList
+    {
+        Node headtop;
+        public void Equeue(int data)
+        {
+            Node node = new Node(data);
+            if (this.headtop == null)
+            {
+                this.headtop = node;
+            }
+            else
+            {
+                Node temp = headtop;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into Linked List", node.data);
+        }
+        public void Display()
+        {
+            Node temp = this.headtop;
+            if (temp ==null)
+            {
+                Console.WriteLine("stacklist is empty");
+            }
+            while (temp !=null)
+            {
+                Console.WriteLine(temp.data +" ");
+                temp =temp.next;
+            }
+        }
+        public void Dequeue()
+        {
+            if (this.headtop==null)
+            {
+                Console.WriteLine("list is empty");
+            }
+            Console.WriteLine("data is top position "+this.headtop.data);
+            this.headtop = this.headtop.next;
+        }
+        public void Empty()
+        {
+            while (this.headtop!=null)
+            {
+                Dequeue();
+
+            }
+        }
+        public void Size()
+        {
+            Node temp = this.headtop;
+            int count = 0;
+            if (temp == null)
+            {
+                Console.WriteLine("linked list is empty");
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data+" ");
+                temp = temp.next;
+                count++;
+            }
+            Console.WriteLine("size of stack list is "+count);
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ StackLinkedList.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedStackAndQueProblem
+{
+    public class StackLinkedList
+    {
+        Node headtop;
+        public StackLinkedList()
+        {
+            this.headtop = null;
+        }
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.headtop == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.headtop;
+            }
+            this.headtop =node;
+            Console.WriteLine("added the stack value "+value);
+        }
+        public void Display()
+        {
+            Node temp = this.headtop;
+            if(temp ==null)
+            {
+                Console.WriteLine("stacklist is empty");
+            }
+            while(temp !=null)
+            {
+                Console.WriteLine(temp.data +" ");
+                temp =temp.next;
+            }
+        }
+        public void peek()
+        {
+            if (this.headtop==null)
+            {
+                Console.WriteLine("list is empty");
+            }
+            Console.WriteLine("{0} is the top of the stack list",this.headtop.data);
+        }
+        public void Pop()
+        {
+            if (this.headtop==null)
+            {
+                Console.WriteLine("list is empty");
+            }
+            Console.WriteLine("value is pop "+this.headtop.data);
+            this.headtop = this.headtop.next;
+        }
+        public void Empty()
+        {
+            while (this.headtop!=null)
+            {
+                peek();
+                Pop();
+            }
+        }
+        public void Size()
+        {
+            Node temp = this.headtop;
+            int count = 0;
+            if (temp == null)
+            {
+                Console.WriteLine("linked list is empty");
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data+" ");
+                temp = temp.next;
+                count++;
+            }
+            Console.WriteLine("size of stack list is "+count); 
+
+
+
+
+        }
+    }
+}
+            
