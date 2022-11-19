edac28c3301f63bea0e51b7a57e47181461239ec
Snehal979 <bansodsnehal535@gmail.com>
2022-11-19 22:32:56 +0530

    Main Program

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LinkedStackAndQueProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            StackLinkedList stackLinkedList = new StackLinkedList();
+            stackLinkedList.Push(70);
+            stackLinkedList.Push(30);
+            stackLinkedList.Push(56);
+            stackLinkedList.Display();
+        }
+    }
+}
