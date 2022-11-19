edd827aba8c287386d659b310756e2ac167b548a
Snehal979 <bansodsnehal535@gmail.com>
2022-11-19 22:35:00 +0530

    push method and display method

---
+++ StackLinkedList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedStackAndQueProblem
+{
+    public class StackLinkedList
+    {
+        Node headtop;
+        public StackLinkedList()
+        {
+            this.headtop = null;
+        }
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.headtop == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.headtop;
+            }
+            this.headtop =node;
+            Console.WriteLine("added the stack value "+value);
+        }
+        public void Display()
+        {
+            Node temp = this.headtop;
+            if(temp ==null)
+            {
+                Console.WriteLine("stacklist is empty");
+            }
+            while(temp !=null)
+            {
+                Console.WriteLine(temp.data +" ");
+                temp =temp.next;
+            }
+        }
+
+    }
+}
+            
